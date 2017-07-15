1c39fb7029aa156ceb18e0049014d7b9e791823c
mofas223 <mofas223@gmail.com>
2017-07-15 16:55:20 +0800

    add note

--- ch1_3.scm
+++ ch1_3.scm
@@ -41,3 +41,28 @@
   (try first-guess))
 
 (fixed-point cos 1.0)
+
+
+; derivative
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            average-damp
+                            1.0))
+
+
+
