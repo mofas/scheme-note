d86266fa8630af7bdedd570207b36362dbdb7b0a
mofas223 <mofas223@gmail.com>
2017-07-16 21:02:06 +0800

    fix ex 2.18

--- ex_2_18.scm
+++ ex_2_18.scm
@@ -1,7 +1,11 @@
 (define (reverse items)
-  (if (null? items)
-      nil
-      cons(reverse(cdr(items)), car(items))
-      ))
+  (define (iter xs result)
+    (if (null? xs)
+        result
+        (iter (cdr xs) (cons (car xs) result))
+        )
+    )
+  (iter items '())
+)
 
 (reverse (list 1 4 9 16 25))
