c35f9b56cac0efbcff252b70257a5f7c1a48c69f
mofas223 <mofas223@gmail.com>
2017-07-16 22:31:08 +0800

    better 2.23 sol

--- ex_2_23.scm
+++ ex_2_23.scm
@@ -1,25 +1,11 @@
 
 
-; the reason we need let declaration is otherwise it will try to excute
-; previous return value
-
-;(define (for-each proc items)
-;  (let (tail (cdr items))
-;    (proc (car items))
-;    (if (not (null? tail))
-;        ((for-each proc tail))
-;        )
-;    )
-;  )
-
-
 (define (for-each proc items)
   (proc (car items))
   (if (not (null? (cdr items)))
       ((for-each proc (cdr items)))
       )
   )
-
 
 
 (for-each (lambda (x) (newline) (display x))
