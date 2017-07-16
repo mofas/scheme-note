aba2d947144a66d780d3f0be3d99d7b65076079d
mofas223 <mofas223@gmail.com>
2017-07-16 21:02:33 +0800

    fix ex 2.17

--- ex_2_17.scm
+++ ex_2_17.scm
@@ -1,10 +1,11 @@
 (define (last-pair items)
   (define (last-pair-iter a prev)
-    (if (null? a))
-    prev
-    (last-pair-iter cdr(a) car(a))
+    (if (null? a)
+        prev
+        (last-pair-iter (cdr a) (car a))
+        )
     )
-  (last-pair-iter items, nil)
+  (last-pair-iter (cdr items) (car items))
   )
 
 (last-pair (list 23 72 149 34))
