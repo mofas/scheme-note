bdc9211371cf4206bf2251a0fc7399ed257bd24d
mofas223 <mofas223@gmail.com>
2017-07-21 11:40:24 +0800

    finish ex 2 56

--- ch2.scm
+++ ch2.scm
@@ -183,3 +183,20 @@
         ((eq? item (car x)) x)
         (else (memq item (cdr x)))))
 
+; differentiate
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+          (error "unknown expression type -- DERIV" exp))))
+
---
+++ ex_2_56.scm
@@ -0,0 +1,11 @@
+; see ch2_deriv.scm
+(define (make-exponentiation base exponent)
+  (cond ((=number? exponent 0) 1)
+        ((=number? exponent 1) base)
+        (else (list '** base exponent))))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base p) (cadr p))
+(define (exponent p) (caddr p))
