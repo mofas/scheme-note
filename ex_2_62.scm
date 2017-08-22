316d8fe55617b423eecbd2d78b6f173670f8bcb6
mofas223 <mofas223@gmail.com>
2017-08-22 14:36:20 -0400

    finish little scheme to ch5

--- ex_2_62.scm
+++ ex_2_62.scm
@@ -30,4 +30,6 @@
         ((= (car set1) (car set2)) (cons (car set1) (union-set (cdr set1) (cdr set2))))
         ((> (car set1) (car set2)) (cons (car set2) (union-set set1 (cdr set2)))
         (else (cons (car set1) (union-set (cdr set1) set2))
-    )
+    ))))
+
+
--- littleScheme.scm
+++ littleScheme.scm
@@ -33,8 +33,41 @@
         ((eq? old (car lat)) (cons new lat))
         (else (cons (car lat) (insertR new lod (cdr lat))))))
 
-
 (define (multirember a lat)
   (cond ((null? lat) #f)
         ((eq? a (car lat)) (multirember a (cdr lat)))
         (else (cons (car lat) (multirember a (cdr lat))))))
+
+(define (addtup tup)
+  (cond ((null? tup) '())
+        (else (+ (car tup) (addtup (cdr tup))))))
+
+(define (tup+ tup1 tup2)
+  (cond ((or (null? tup1) (null? tup2)) '())
+        (else (cons (+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2))))))
+
+
+(define (occur* a lat)
+  (cond ((null? lat) 0)
+        ((atom? (car lat))
+         (cond (eq? a (car lat))
+               (add1 (occur* a (cdr lat)))
+               (else (occur* a (cdr lat)))))
+        (else (+ (occur* a (car lat)) (occur* a (cdr lat)))))
+
+
+(define (subst* new old l)
+  (cond ((null? l) '())
+        ((atom? (car l))
+         (cond (eq? old (car l))
+               (cons new (subst* new old (cdr l)))
+               (else (cons old (subst* new old (cdr l)))))
+         )
+        (else (cons (subst* new old (car l) (subst* new old (cdr l)))))))
+
+
+(define (member* a l)
+  (cond ((null? l) #f)
+        ((atom? (car l))
+         (or ((eq? a (car l)) #t) (member* a (cdr l))))
+        (else (or (member* a (car l) (member* a (cdr l))))))
