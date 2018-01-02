88c72c2de5724d4cdf4ca88a3d3f958b76e8a261
mofas223 <mofas223@gmail.com>
2018-01-02 12:26:55 -0600

    refactor simulator

--- ch3_simulator.rkt
+++ ch3_simulator.rkt
@@ -1,6 +1,8 @@
 #lang racket
-
 (require compatibility/mlist)
+
+(provide (all-defined-out))
+
 
 ;; time segment
 
@@ -246,29 +248,3 @@
                  (display " New-value = ")
                  (displayln (get-signal wire)))))
 
-
-;; small app
-
-(define (half-adder a b s c)
-  (let ((d (make-wire)) (e (make-wire)))
-    (or-gate a b d)
-    (and-gate a b c)
-    (inverter c e)
-    (and-gate d e s)
-    'ok))
-
-
-(define input-1 (make-wire))
-(define input-2 (make-wire))
-(define sum (make-wire))
-(define carry (make-wire))
-
-(probe 'sum sum)
-(probe 'carry carry)
-
-(half-adder input-1 input-2 sum carry)
-(set-signal! input-1 1)
-(propagate)
-
-(set-signal! input-2 1)
-(propagate)
---
+++ ch3_simulator_app.rkt
@@ -0,0 +1,32 @@
+#lang racket
+(require compatibility/mlist)
+
+(require "./ch3_simulator.rkt")
+
+
+
+;; small app
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+(probe 'sum sum)
+(probe 'carry carry)
+
+(half-adder input-1 input-2 sum carry)
+(set-signal! input-1 1)
+(propagate)
+
+(set-signal! input-2 1)
+(propagate)
