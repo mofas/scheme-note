03deae2a3d9a4e0fb4e4cfeec907aa0fcf5e370f
mofas223 <mofas223@gmail.com>
2018-01-06 23:50:13 -0500

    add ch3 streams

---
+++ ch3_streams.rkt
@@ -0,0 +1,60 @@
+#lang racket
+(require compatibility/mlist)
+
+(provide (all-defined-out))
+
+
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+          (begin (set! result (proc))
+                 (set! already-run? true)
+                 result)
+          result))))
+
+(define delay (delay p) (memo-proc (λ() p)))
+
+(define (force delayed-object)
+  (delayed-object))
+
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      ’done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+#;
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map 
+              (cons proc (map stream-cdr argstreams))))))
